804a483472d51ec40310be2396792265135ba321
Pedro <pedrohalmeidamendonca@gmail.com>
2023-03-15 22:36:48 -0300

    Requisito 02

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -8,4 +8,15 @@ router.use(express.json());
 
 router.get('/', async (_req, res) => res.status(200).json(await readTalkers()));
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkers = await readTalkers();
+  const talker = talkers.find((t) => t.id === +id);
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(talker);
+});
+
 module.exports = router;
