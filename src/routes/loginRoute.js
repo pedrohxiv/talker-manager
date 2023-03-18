e4cdfc9294268a887d5dfe92961ba712f07d571a
Pedro <pedrohalmeidamendonca@gmail.com>
2023-03-18 14:28:32 -0300

    Requisito 04

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -6,9 +6,28 @@ const router = express.Router();
 
 router.use(express.json());
 
-router.post('/', (_req, res) => {
-    const token = randomToken();
-    return res.status(200).json({ token });
+const loginValidation = (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+};
+
+router.post('/', loginValidation, (_req, res) => {
+  const token = randomToken();
+  return res.status(200).json({ token });
 });
 
 module.exports = router;
